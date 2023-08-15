3d7be66a810d71b99098c300f80cb879504eddbe
kwonyongil <kkwonyi95@gmail.com>
2023-08-15 23:10:24 +0900

    signin finish

--- PittaSNSApp/app/src/main/java/com/team8/pittasnsapp/SignInActivity.kt
+++ PittaSNSApp/app/src/main/java/com/team8/pittasnsapp/SignInActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,15 +13,28 @@ class SignInActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_in)
 
         initViews()
+
     }
 
     private fun initViews() {
-        findViewById<Button>(R.id.button).setOnClickListener {
-            startActivity(Intent(this,SignUpActivity::class.java))
+        val idText = findViewById<EditText>(R.id.id_edit_text)
+        val pwText = findViewById<EditText>(R.id.pw_edit_text)
+
+        findViewById<Button>(R.id.login_btn).setOnClickListener {
+            val id = idText.text.toString()
+            val pw = pwText.text.toString()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("textFromId", id)
+            if (id.isEmpty() || pw.isEmpty()) {
+                Toast.makeText(this, "아이디/비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+            }
         }
 
-        findViewById<Button>(R.id.button2).setOnClickListener {
-            startActivity(Intent(this,MainActivity::class.java))
+        findViewById<Button>(R.id.signup_btn).setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
         }
     }
 }
