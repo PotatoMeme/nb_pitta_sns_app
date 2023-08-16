030d4e490914902ff404e15c052b06c4280412e7
PotatoMeme <kshk0897@gmail.com>
2023-08-16 15:04:41 +0900

    [FEAT] : ⚙️ main page setting design

--- PittaSNSApp/app/src/main/java/com/team8/pittasnsapp/SettingFragment.kt
+++ PittaSNSApp/app/src/main/java/com/team8/pittasnsapp/SettingFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class SettingFragment : Fragment() {
     companion object {
@@ -24,7 +26,21 @@ class SettingFragment : Fragment() {
     }
 
     private fun initView(view: View) {
-
-
+        val listView: ListView = view.findViewById<ListView>(R.id.list_view)
+        val settingMenuTitleArray: Array<String> =
+            arrayOf(
+                "로그아웃",
+                "내 정보",
+                "폰트",
+                "테마",
+            )
+        val arrayAdapter = context?.let {
+            ArrayAdapter(
+                it,
+                android.R.layout.simple_list_item_1,
+                settingMenuTitleArray
+            )
+        }
+        listView.adapter = arrayAdapter
     }
 }
