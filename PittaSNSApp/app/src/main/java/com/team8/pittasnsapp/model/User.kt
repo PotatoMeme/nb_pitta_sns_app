e06a4d992be4ef94c2a00028a27a8f11f58b473a
PotatoMeme <kshk0897@gmail.com>
2023-08-17 15:50:18 +0900

    [FEAT] : ⚙️ User Detail Activity data settings

--- PittaSNSApp/app/src/main/java/com/team8/pittasnsapp/model/User.kt
+++ PittaSNSApp/app/src/main/java/com/team8/pittasnsapp/model/User.kt
@@ -3,5 +3,6 @@ package com.team8.pittasnsapp.model
 data class User(
     val id: Int,
     val name: String,
-    val userImgUrl : String // 이미지 Url
+    val message: String,
+    val userImgUrl: String, // 이미지 Url
 )
